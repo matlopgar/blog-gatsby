834eae06694ab83b0328233ca79b40e941e54d3b
Matheus Garcia <matheuslopesgarcia@gmail.com>
2021-12-07 13:23:17 -0300

    sidebar with icons

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,17 @@
+const links = [
+    {
+        label: 'Github',
+        url: 'https://github.com/matlopgar'
+    },
+
+    {
+        label: 'Twitter',
+        url: 'https://twitter.com/matheusgarcia'
+    },
+    {
+        label: 'Linkedin',
+        url: 'https://linkedin.com/matheuslopesgarcia'
+    }
+]
+
+export default links
