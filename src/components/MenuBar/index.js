ff91dec006b8524fa747642a1cfeb27de3c729a7
Matheus Garcia <matheuslopesgarcia@gmail.com>
2021-12-07 18:00:34 -0300

    Menubar and PostItem data

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -2,16 +2,15 @@ import React from "react"
 
 import { Home } from '@styled-icons/boxicons-solid/Home'
 import { SearchAlt2 as Search } from '@styled-icons/boxicons-regular/SearchAlt2'
-import { UpArrowAlt as Arrow } from '@styled-icons/boxicons-regular/UpArrowAlt'
 import { Bulb as Light } from '@styled-icons/boxicons-regular/Bulb'
-import { Grid } from '@styled-icons/boxicons-solid/Grid'
+
 
 import * as S from "./styled"
 
 const MenuBar = () => (
   <S.MenuBarWrapper>
     <S.MenuBarGroup>
-      <S.MenuBarLink to="/" title="Voltar para Home">
+      <S.MenuBarLink to="/" title="Home">
         <S.MenuBarItem>
           <Home />
         </S.MenuBarItem>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,15 @@ import PostItem from "../components/PostItem"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <PostItem />
+    <PostItem 
+      slug="/about/"
+      background="#000000"
+      category="Dog"
+      date="7 Dec 2020"
+      timeToRead="5"
+      title="Demystify NgRx"
+      description="Angular state manager is not rocket science"
+    />
   </Layout>
 )
 
